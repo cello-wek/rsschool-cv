d970da0593854f3c73f9fab16894b408621589a1
cello-wek <falya500@gmail.com>
2021-09-20 20:30:42 +0300

    feat: change files

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuBtn = document.querySelector('.menu__btn');
-if(menuBtn){
-	const menuNav = document.querySelector('.nav-menu');
-	menuBtn.addEventListener("click", function(e){
+const menuItems = document.querySelectorAll('.nav-link');
+const menuNav = document.querySelector('.nav-menu');
+
+if (menuBtn) {
+
+	menuBtn.addEventListener("click", function (e) {
 		menuBtn.classList.toggle('--active');
 		menuNav.classList.toggle('--active');
-	});
 
+	});
+}
+if (menuItems) {
+	menuItems.forEach(el => el.addEventListener("click", function (e) {
+		menuNav.classList.toggle('--active');
+	}));
 }
